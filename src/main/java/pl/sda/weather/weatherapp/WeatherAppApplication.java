e7ffb6bc3375b42e5b15316128ac226b58b25f4b
Tomasz Owczarek <tomasz.owczarek@tomtom.com>
2021-07-24 12:29:55 +0200

    Init

---
+++ src/main/java/pl/sda/weather/weatherapp/WeatherAppApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.weather.weatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAppApplication.class, args);
+	}
+
+}
